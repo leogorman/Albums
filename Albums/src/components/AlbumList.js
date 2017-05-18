b04ab0f4649d528994476bf6355b21d1ba0c8bf2
Liam O Gorman <L.ogorman@mycit.ie>
2017-05-18 22:45:21 +0100

    Code added to pull albums from api on heroku

--- Albums/src/components/AlbumList.js
+++ Albums/src/components/AlbumList.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
-	return (
+class AlbumList extends Component {
+	//Load in data here, lifecycle method is automatically run when class is used
+	componentWillMount(){
+		console.log('here');
+		//http request to api retrieving json list of albums
+		axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => console.log('response: ' + response));	
+	}
+
+	render() {
+		return (
 		<View>
-			<Text>Album List!!!</Text>
+			<Text>Album List</Text>
 		</View>
 		);
-	};
+	}
+};
 
 export default AlbumList;
