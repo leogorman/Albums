faa9ab678e213dc8607b5491d4fb316eaa2725c1
Liam O Gorman <L.ogorman@mycit.ie>
2017-05-19 01:17:16 +0100

    Updated AlbumList.js -> Getting response from api and rendering json array of albums to screen

--- Albums/src/components/AlbumList.js
+++ Albums/src/components/AlbumList.js
@@ -3,20 +3,29 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+	//inital component state
+	state = { albums: [] };
+
 	//Load in data here, lifecycle method is automatically run when class is used
 	componentWillMount(){
-		console.log('here');
 		//http request to api retrieving json list of albums
-		axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => console.log('response: ' + response));	
+		axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+		.then(response => this.setState({albums: response.data}));	
+	}
+
+	renderAlbums(){
+		return this.state.albums.map(album => <Text>{album.title}</Text>);
 	}
 
 	render() {
+
 		return (
-		<View>
-			<Text>Album List</Text>
-		</View>
+			<View>
+				{this.renderAlbums()}
+			</View>
 		);
 	}
 };
 
 export default AlbumList;
+
